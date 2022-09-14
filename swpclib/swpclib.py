3fbca17655de341f299db015aedb6e9a4953f26e
Tyler F. Carr <tcarwash@gmail.com>
2022-09-14 21:45:07 +0000

    add get_sfi

--- swpclib/swpclib.py
+++ swpclib/swpclib.py
@@ -1,1 +1,23 @@
-"""Main module."""
+import requests
+
+url = "https://services.swpc.noaa.gov/json/f107_cm_flux.json"
+base_url = "https://services.swpc.noaa.gov/json/"
+
+
+class Runner:
+    def __init__(self):
+        self.base_url = base_url
+
+    def get_sfi(self):
+        response = requests.get(base_url + "f107_cm_flux.json").json()
+        data = {
+            "sfi": int(response[0]["flux"]),
+            "timestamp": response[0]["time_tag"],
+        }
+
+        return data
+
+
+if __name__ == "__main__":
+    swpc = Runner()
+    print(swpc.get_sfi())
