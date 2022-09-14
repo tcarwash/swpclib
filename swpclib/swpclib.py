5f22d46fb624e4379689fe3eaa88516a3b0067bb
Tyler F. Carr <tcarwash@gmail.com>
2022-09-14 22:19:54 +0000

    slice as arg for sfi

--- swpclib/swpclib.py
+++ swpclib/swpclib.py
@@ -8,14 +8,18 @@ class Runner:
     def __init__(self):
         self.base_url = base_url
 
-    def get_sfi(self):
+    def get_sfi(self, start=0, end=1, step=1):
         response = requests.get(base_url + "f107_cm_flux.json").json()
-        data = {
-            "sfi": int(response[0]["flux"]),
-            "timestamp": response[0]["time_tag"],
-        }
+        data_range = slice(start, end, step)
+        sfi_data = [
+            {
+                "sfi": int(item["flux"]),
+                "timestamp": item["time_tag"],
+            }
+            for item in response[data_range]
+        ]
 
-        return data
+        return sfi_data
 
 
 if __name__ == "__main__":
