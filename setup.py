6e888ba17a5a7575c28c9ef14c0f270f3be72c3b
Tyler F. Carr <tcarwash@gmail.com>
2022-09-24 01:39:35 +0000

    add long_description

--- setup.py
+++ setup.py
@@ -4,10 +4,17 @@ test_requirements = [
     "pytest>=3",
 ]
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 if __name__ == "__main__":
     setup(
         setup_requires=["setuptools-git-versioning"],
         test_suite="test",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         tests_require=test_requirements,
         install_requires=["aiohttp==3.8.3"],
         name="swpclib",
